0d23f3371f7a632929392b8fba3b1de1f6b072a3
Erik Williamson <me@erik.tw>
2020-12-08 14:15:26 -0800

    try adding lambda

--- example.tf
+++ example.tf
@@ -28,48 +28,41 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_s3_bucket_object" "file_upload" {
-  bucket = "ertw-terraform-serverless-example"
-  key    = "example.zip"
-  source = filebase64("${path.module}/example.zip")
-  etag   = filemd5("${path.module}/example.zip")
+resource "aws_lambda_function" "example" {
+   function_name = "ServerlessExample"
+
+   # The bucket name as created earlier with "aws s3api create-bucket"
+   s3_bucket = "ertw-terraform-serverless-example"
+   s3_key    = "example.zip"
+
+   # "main" is the filename within the zip file (main.js) and "handler"
+   # is the name of the property under which the handler function was
+   # exported in that file.
+   handler = "main.handler"
+   runtime = "nodejs10.x"
+
+   role = aws_iam_role.lambda_exec.arn
 }
 
-#resource "aws_lambda_function" "example" {
-#   function_name = "ServerlessExample"
-#
-#   # The bucket name as created earlier with "aws s3api create-bucket"
-#   s3_bucket = "ertw-terraform-serverless-example"
-#   s3_key    = "v1.0.0/example.zip"
-#
-#   # "main" is the filename within the zip file (main.js) and "handler"
-#   # is the name of the property under which the handler function was
-#   # exported in that file.
-#   handler = "main.handler"
-#   runtime = "nodejs10.x"
-#
-#   role = aws_iam_role.lambda_exec.arn
-#}
-#
-# # IAM role which dictates what other AWS services the Lambda function
-# # may access.
-#resource "aws_iam_role" "lambda_exec" {
-#   name = "serverless_example_lambda"
-#
-#   assume_role_policy = <<EOF
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Action": "sts:AssumeRole",
-#      "Principal": {
-#        "Service": "lambda.amazonaws.com"
-#      },
-#      "Effect": "Allow",
-#      "Sid": ""
-#    }
-#  ]
-#}
-#EOF
-#
-#}
+ # IAM role which dictates what other AWS services the Lambda function
+ # may access.
+resource "aws_iam_role" "lambda_exec" {
+   name = "serverless_example_lambda"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
