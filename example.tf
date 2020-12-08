d830898b725913f631220c09d23886dbc0858248
Erik Williamson <me@erik.tw>
2020-12-08 13:26:26 -0800

    oops fix typo

--- example.tf
+++ example.tf
@@ -19,7 +19,7 @@ resource "aws_sqs_queue" "terraform_queue" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "terreform-serverless-example"
+  bucket = "terraform-serverless-example"
   acl    = "private"
 
   tags = {
